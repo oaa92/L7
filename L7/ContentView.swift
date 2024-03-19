419d67e4dc4627dbe36c07346a64f6a43de5c47c
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-19 19:03:59 +0700

    Initial Commit

---
+++ L7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  L7
+//
+//  Created by Anatoliy Odinetskiy on 19.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ L7/L7App.swift
@@ -0,0 +1,17 @@
+//
+//  L7App.swift
+//  L7
+//
+//  Created by Anatoliy Odinetskiy on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct L7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
